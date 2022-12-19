79cbc673dea9b53d423c446100c4db35c17a8ae7
Morel-D <morelDen@gmail.com>
2022-12-19 12:01:35 -0800

    Setting Up TodoList

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
+import Todo from "./Components/Todo";
+import React, { Components } from 'react';
+
 
 function App() {
+
+
+  const todos = [
+    { id: 1, content: "Buy some milk for the breakfast" },
+    { id: 2, content: "Play video games all day" },
+    { id: 3, content: "Go to the gym" },
+
+  ]
+  
+
+  const deleteTodo = (id) => {
+    const list = todos.filter((todo) => {
+      return todo.id !== id
+    });
+
+    return list;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-       hello
       </header>
+
+      <div className="container center todo-app py-5">
+        <h2 className="pink-text"><b>Todo List</b></h2>
+        <Todo  todos={todos} deleteTodo ={ deleteTodo } />
+      </div>
+
     </div>
   );
 }
---
+++ client/src/Components/Todo.jsx
@@ -0,0 +1,28 @@
+
+
+const Todo = ({ todos, deleteTodo }) => {
+
+
+    const todoList = todos.length ? (
+        todos.map((todo) => {
+            return (
+            <div className="collection-item" key={todo.id}>
+                <span onClick={() => { deleteTodo(todo.id) }}>{todo.content}</span>
+            </div>
+           )
+        })
+    ) : (
+           
+        <p className="center">Empty agenda</p>
+       
+    )
+
+
+    return ( 
+        <div className="todos collection">
+            { todoList }
+        </div>
+     );
+}
+ 
+export default Todo;
